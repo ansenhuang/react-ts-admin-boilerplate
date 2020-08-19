9ed135c177207ab122d31cd5f3fa97c1b91e79dc
huangancheng <huangancheng@bytedance.com>
2020-08-19 18:12:19 +0800

    feat: add byte design

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,35 @@ module.exports = {
         libraryDirectory: isTestEnv ? 'lib' : 'es',
         style: true,
       },
+      'antd',
+    ],
+    [
+      'import',
+      {
+        libraryName: '@byte-design/ui',
+        libraryDirectory: isTestEnv ? 'lib/components' : 'es/components',
+        transformToDefaultImport: false,
+        style: true,
+      },
+      '@byte-design/ui'
+    ],
+    [
+      'import',
+      {
+        libraryName: '@byte-design/icons',
+        libraryDirectory: 'icons',
+        camel2DashComponentName: false,
+      },
+      '@byte-design/icons'
+    ],
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'lodash'
     ],
   ],
 };
